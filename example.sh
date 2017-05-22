6db014637fc95d6d2f1116dbdeb63916ec10f2bf
Jesse Lang <jesse@jesselang.com>
2017-05-21 22:27:17 -0500

    Half-decent example

--- example.sh
+++ example.sh
@@ -3,3 +3,15 @@
 source demo.sh
 
 c This is a simple example of how to use demo.sh
+c "Let's hold after this comment. You'll need to press a key to continue"
+hold
+c "Hey, what time is it? Let's find out."
+x date
+c "The terminal is getting messy. Let's clean up. Press a key to continue."
+hold
+clear
+c "Time for a live demo! When you're finished, type 'exit' or Ctrl-D"
+shell
+c "Great job on the live portion of your demo! One more time check before we go."
+x date
+c "Thanks for all the fish!"
