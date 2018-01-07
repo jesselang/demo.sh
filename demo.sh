b3e1822d0fc147a678ba4133cdefb57d685a0019
Jesse Lang <jesse@jesselang.com>
2018-01-06 19:18:16 -0600

    Environment variables for demo prompts and colors

--- demo.sh
+++ demo.sh
@@ -10,16 +10,48 @@ if [ $_echo_en -eq 0 ]; then
 fi
 unset _echo_en
 
-SPEED='0.1'
-MUTED='\033[38;5;242m'
-NORMAL='\033[0m'
-DEMO_PROMPT=demo
+# colors - https://en.wikipedia.org/wiki/ANSI_escape_code#8-bit
+_clr_muted='\e[38;5;242m'
+_clr_hold='\e[38;5;1m'
+_clr_live='\e[38;5;2m'
+_clr_normal='\e[m'
+
+# these variables can be overwritten
+# default speed
+DEMO_SPEED=${DEMO_SPEED:-0.1}
+
+# default prompt text
+DEMO_PROMPT=${DEMO_PROMPT:-demo}
+DEMO_PROMPT_HOLD=${DEMO_PROMPT_HOLD:-hold}
+DEMO_PROMPT_LIVE=${DEMO_PROMPT_LIVE:-live}
+
+# default colors
+DEMO_CLR_PROMPT=${DEMO_PROMPT_CLR:-$_clr_muted}
+DEMO_CLR_HOLD=${DEMO_CLR_HOLD:-$_clr_hold}
+DEMO_CLR_LIVE=${DEMO_CLR_LIVE:-$_clr_live}
+
+# default prompts
+DEMO_PS1="${DEMO_CLR_PROMPT}${DEMO_PROMPT}>${_clr_normal} "
+DEMO_PS1_HOLD="${DEMO_CLR_HOLD}-- ${DEMO_PROMPT_HOLD} --${_clr_normal} "
+DEMO_PS1_LIVE="${DEMO_CLR_LIVE}${DEMO_PROMPT_LIVE}>${_clr_normal} "
 
 _prompt() {
     _clear_line
-    echo -en "${MUTED}${1:-${DEMO_PROMPT}}>${NORMAL} "
+    case "$1" in
+        hold)
+            echo -en "${DEMO_PS1_HOLD}"
+            ;;
+        '')
+            echo -en "${DEMO_PS1}"
+            ;;
+        *)
+            echo "error: _prompt() doesn't know how to handle '$1'" >&2
+            exit 1
+            ;;
+    esac
 }
 
+# ANSI escape codes - http://www.climagic.org/mirrors/VT100_Escape_Codes.html
 _clear_line() {
     echo -en '\r'      # carriage return
     echo -en '\033[2K' # clear entire line
@@ -28,15 +60,15 @@ _clear_line() {
 _write() {
     _prompt
     # shellcheck disable=SC2034
-    for _sleep in 1 2 3 4 5 6; do
-        sleep "${DEMO_SPEED:-$SPEED}"
+    for _sleep in {1..6}; do
+        sleep "${DEMO_SPEED}"
     done
 
     output=$*
 
     for (( i=0; i<${#output}; i++ )); do
         echo -n "${output:$i:1}"
-        sleep "${DEMO_SPEED-$SPEED}"
+        sleep "${DEMO_SPEED}"
     done
 
     echo
@@ -84,7 +116,7 @@ hold() {
 shell() {
     trap - EXIT
     _clear_line
-    PS1="${MUTED}live>${NORMAL} " bash --noprofile --norc
+    PS1="${DEMO_PS1_LIVE}" bash --noprofile --norc
     echo -en '\033[1A' # move up 1 line
     _prompt
     trap _clear_line EXIT
@@ -93,7 +125,7 @@ shell() {
 if [ "$0" = "${BASH_SOURCE[0]}" ]; then
     DEMO_PROMPT=demo.sh
     c Mix scripted and live demos with ease
-    DEMO_SPEED='0.025'
+    DEMO_SPEED=${DEMO_SPEED:-0.025}
     c 'Comments and commands appear to be "typed" which offers time'
     c 'for presenters to talk through what is happening as it happens'
     c
